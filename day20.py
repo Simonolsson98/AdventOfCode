1f34e62e13ed15c6668471f319c5f7b59ea46663
Simon Olsson <Simol@student.chalmers.se>
2021-03-21 15:35:57 +0100

    Update day20.py

--- day20.py
+++ day20.py
@@ -82,4 +82,4 @@ def main():
 if __name__ == '__main__':
     start_time = time.time()
     returnVal = main() 
-    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer = 15405893262491 
+    print(f"answer = {returnVal}, execution time: {time.time() - start_time} seconds") #answer =  
