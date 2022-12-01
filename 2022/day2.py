258ff9a2ded06295e6ed05cc6bb6436dc18dd0cc
Simon <simol@student.chalmers.se>
2022-12-01 18:04:44 +0100

    day2 src

---
+++ 2022/day2.py
@@ -0,0 +1,16 @@
+input = open("day2_input.txt")
+lines = []
+i = input.readline()
+subtotal = 0;
+
+while i:
+	lines.append(i)
+	i = input.readline()
+
+# part 1: 
+		
+print("solution for part 1: " + str())
+
+# part 2:  
+
+print("solution for part 2: " + str())
