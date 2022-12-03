6ef9095c56d5d2ecb858c234fcddeeb1a00271e2
Simon <simol@student.chalmers.se>
2022-12-03 13:10:26 +0100

    day3

---
+++ 2022/day3.py
@@ -0,0 +1,11 @@
+input = open("day3_input.txt")
+i = input.readline()
+
+while i:
+	i = input.readline()
+
+# part 1: 
+print("solution for part 1: " + str())
+
+# part 2:  
+print("solution for part 2: " + str())
