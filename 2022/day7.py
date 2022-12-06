736eb58ea0ed5063c7f76113e97d20fe565facfa
Simon <simol@student.chalmers.se>
2022-12-06 19:19:33 +0100

    day7 template

---
+++ 2022/day7.py
@@ -0,0 +1,18 @@
+input = open("day7_input.txt")
+
+i = input.readline()
+if(i[:-1] == "\n"):
+	i = i[:-1]
+
+while i:
+
+	i = input.readline()
+	if(i[:-1] == "\n"):
+		i = i[:-1]
+
+
+# part 1: 
+print("day7: solution for part 1: " + str())
+
+# part 2:  
+print("day7: solution for part 2: " + str())
