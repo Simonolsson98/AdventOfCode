1212a5e3d252a0f6195852a0ce0b9332fa4d0a3b
Simon <simol@student.chalmers.se>
2022-12-06 01:25:57 +0100

    day6 template

---
+++ 2022/day6.py
@@ -0,0 +1,18 @@
+input = open("day6_input.txt")
+
+i = input.readline()
+if(i[:-1] == "\n"):
+	i = i[:-1]
+
+while i:
+	
+	i = input.readline()
+	if(i[:-1] == "\n"):
+		i = i[:-1]
+
+
+# part 1: 
+print("day6: solution for part 1: " + str())
+
+# part 2:  
+print("day6: solution for part 2: " + str())
