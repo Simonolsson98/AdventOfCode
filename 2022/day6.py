c5c5563d8dd5b0b93754abf97231775d4d38a324
Simon <simol@student.chalmers.se>
2022-12-06 19:11:13 +0100

    day6

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,17 +1,20 @@
-input = open("day6_input.txt")
+input_str = open("day6_input.txt").read()
+recent_part1 = [None, input_str[0], input_str[1], input_str[2]]
+recent_part2 = [None, input_str[0], input_str[1], input_str[2], input_str[3], input_str[4], input_str[5], input_str[6], input_str[7], input_str[8], input_str[9], input_str[10], input_str[11], input_str[12]]
+result = 0
 
-i = input.readline()
-if(i[:-1] == "\n"):
-	i = i[:-1]
+def FindSolution(i, recent):
+	for count, char in enumerate(input_str[(i-1):]):
+		for x in range(i-1):
+			recent[x] = recent[x + 1]
 
-while i:
-	
-	i = input.readline()
-	if(i[:-1] == "\n"):
-		i = i[:-1]
+		recent[i-1] = char
+
+		if(len(set(recent)) == len(recent)):
+			return count + i
 
 # part 1: 
-print("day6: solution for part 1: " + str())
+print("day6: solution for part 1: " + str(FindSolution(4, recent_part1)))
 
 # part 2:  
-print("day6: solution for part 2: " + str())
+print("day6: solution for part 2: " + str(FindSolution(14, recent_part2)))
