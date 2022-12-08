81815dad73f780ad7294d783ad8e787b9591c646
Simon <simol@student.chalmers.se>
2022-12-08 22:42:28 +0100

    day9

---
+++ 2022/day9.py
@@ -0,0 +1,11 @@
+input = open("day9_input.txt")
+i = input.readline()[:-1]
+
+while i:
+	i = input.readline()[:-1]
+				
+# part 1: 
+print("day9: solution for part 1: " + str())
+
+# part 2:  
+print("day9: solution for part 2: " + str())
