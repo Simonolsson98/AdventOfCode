0c140ac1d3c1dcd50a70fc362ede50b0ce3667db
Simon <simol@student.chalmers.se>
2022-12-08 22:36:33 +0100

    day7

--- 2022/day7.py
+++ 2022/day7.py
@@ -1,47 +1,47 @@
 input = open("day7_input.txt")
 i = input.readline()[:-1]
 
-# somehow allow duplicate dirs..
-asd = [newDict = {"/": 0}]
-
+newDict = {"/": 0}
 prevDirs = []
 currentDir = ""
+
 while i:
 	if(i[:4] == "$ cd"):
 		if(currentDir != "" and i[5:] != ".."):
 			prevDirs.append(currentDir)
-			print(f"adding {currentDir} to prevDirs")
 
-		currentDir = i[5:]
-		if(currentDir == ".."):
+		currentDir += i[5:]
+		if(i[5:] == ".."):
 			currentDir = prevDirs.pop()
-			print(f"going back to {currentDir}")
-
-		print(f"CD TO {currentDir}")
 
 		i = input.readline()[:-1]
 	elif(i[:4] == "$ ls"):
 		i = input.readline()[:-1]
 		while(i != "" and i[0] != "$"):
 			first = i.split(" ")
-			print(f"{first[0]} {first[1]}")
 			if(first[0] == "dir"):
-				newDict.update({first[1]: 0})
+				newDict.update({(currentDir + first[1]): 0})
 			else:
 				newDict[currentDir] += int(first[0])
-				print(f"adding {first[0]} to currentDir: {currentDir}")
 				for prevDir in prevDirs:
 					newDict[prevDir] += int(first[0])
-					print(f"adding {first[0]} to prevDir: {prevDir}")
 			
 			i = input.readline()[:-1]
 
 result = sum([val for val in newDict.values() if val <= 100_000])
 
-print(newDict)
-
 # part 1: 
 print("day7: solution for part 1: " + str(result))
 
+totalUsed = newDict["/"]
+total = 70_000_000
+needed = 30_000_000
+freeUpAmount = needed - (total - totalUsed)
+
+closest = needed
+for val in newDict.values():
+	if((val - freeUpAmount) < closest and (val - freeUpAmount) > 0):
+		closest = val
+
 # part 2:  
-print("day7: solution for part 2: " + str())
+print("day7: solution for part 2: " + str(closest))
