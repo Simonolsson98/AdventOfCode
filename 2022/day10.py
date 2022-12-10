ffada384dfa8896158272e5cf99e3987f6df76b7
Simon <simol@student.chalmers.se>
2022-12-10 14:30:21 +0100

    day10

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,14 +1,33 @@
 input = open("day10_input.txt")
 i = input.readline()[:-1]
 
+register = 1
+cycle = 1
+strength = 0
 
 while i:
-	
+	instr = i.split(" ")
+	if(len(instr) == 1):
+		cycle += 1
+		if(cycle == 20 or (cycle + 20) % 40 == 0):
+			print(f"value of reg: {register} at {cycle}")
+			strength += (cycle * register)
+	else:
+		cycle += 1
+		if(cycle == 20 or (cycle + 20) % 40 == 0):
+			print(f"value of reg: {register} at {cycle}")
+			strength += (cycle * register)
+		cycle += 1
+		register += int(instr[1])
+		if(cycle == 20 or (cycle + 20) % 40 == 0):
+			print(f"value of reg: {register} at {cycle}")
+			strength += (cycle * register)
+
 	i = input.readline()[:-1]
 
 
 # part 1: 
-print("day10: solution for part 1: " + str())
+print("day10: solution for part 1: " + str(strength))
 
 # part 2:  
 print("day10: solution for part 2: " + str())
