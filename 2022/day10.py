ed0abdc71d4d2b68f7b7543520742982f74265e6
Simon <simol@student.chalmers.se>
2022-12-10 14:37:58 +0100

    day10

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,27 +1,29 @@
 input = open("day10_input.txt")
 i = input.readline()[:-1]
 
+
 register = 1
 cycle = 1
 strength = 0
+
+def CheckCycle(cycle, strength, register):
+	if(cycle == 20 or (cycle + 20) % 40 == 0):
+		strength += (cycle * register)
+	return strength
 
 while i:
 	instr = i.split(" ")
 	if(len(instr) == 1):
 		cycle += 1
-		if(cycle == 20 or (cycle + 20) % 40 == 0):
-			strength += (cycle * register)
+		strength = CheckCycle(cycle, strength, register)
 	else:
 		cycle += 1
-		if(cycle == 20 or (cycle + 20) % 40 == 0):
-			strength += (cycle * register)
+		strength = CheckCycle(cycle, strength, register)
 		cycle += 1
 		register += int(instr[1])
-		if(cycle == 20 or (cycle + 20) % 40 == 0):
-			strength += (cycle * register)
+		strength = CheckCycle(cycle, strength, register)
 
 	i = input.readline()[:-1]
-
 
 # part 1: 
 print("day10: solution for part 1: " + str(strength))
