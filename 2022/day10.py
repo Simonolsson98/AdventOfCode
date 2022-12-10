0e92e0996d987a7253ddb2167ba4334002caf435
Simon <simol@student.chalmers.se>
2022-12-10 16:03:02 +0100

    day10

--- 2022/day10.py
+++ 2022/day10.py
@@ -7,38 +7,14 @@ register = 1
 cycle = 1
 strength = 0
 
+height = 6
+width = 40
+grid = [["........................................"] for _ in range(height) ]
+
 def CheckCycle(cycle, strength, register):
 	if(cycle == 20 or (cycle + 20) % 40 == 0):
 		strength += (cycle * register)
 	return strength
-
-while i:
-	instr = i.split(" ")
-	if(len(instr) == 1):
-		cycle += 1
-		strength = CheckCycle(cycle, strength, register)
-	else:
-		cycle += 1
-		strength = CheckCycle(cycle, strength, register)
-		cycle += 1
-		register += int(instr[1])
-		strength = CheckCycle(cycle, strength, register)
-
-	i = input.readline()[:-1]
-
-# part 1: 
-print("day10: solution for part 1: " + str(strength))
-
-input = open("day10_input.txt")
-i = input.readline()[:-1]
-
-register = 1
-cycle = 0
-strength = 0
-
-height = 6
-width = 40
-grid = [["........................................"] for _ in range(height) ]
 
 def CheckIfDrawn(register, cycle):
 	drawn_row = math.floor((cycle - 1) / width)
@@ -52,17 +28,25 @@ def CheckIfDrawn(register, cycle):
 
 while i:
 	instr = i.split(" ")
+	# noop
 	if(len(instr) == 1):
 		cycle += 1
-		CheckIfDrawn(register, cycle)
+		CheckIfDrawn(register, cycle - 1)
+		strength = CheckCycle(cycle, strength, register)
+	#addx int
 	else:
 		cycle += 1
-		CheckIfDrawn(register, cycle)
+		CheckIfDrawn(register, cycle - 1)
+		strength = CheckCycle(cycle, strength, register)
 		cycle += 1
-		CheckIfDrawn(register, cycle)
+		CheckIfDrawn(register, cycle - 1)
 		register += int(instr[1])
+		strength = CheckCycle(cycle, strength, register)
 
 	i = input.readline()[:-1]
+
+# part 1: 
+print("day10: solution for part 1: " + str(strength))
 
 # part 2:  
 print("day10: solution for part 2: ")
