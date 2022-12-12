c4ad8e4107fee270f8c07f8d2ea300c15b14d72f
Simon Olsson <Simon.Olsson@csamhealth.com>
2022-12-12 16:50:00 +0100

    day12

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,9 +1,41 @@
 input = open("day12_input.txt")
 i = input.readline()[:-1]
 
+heightmap = []
 while i:
-	
+	heightmap.append(list(i))	
 	i = input.readline()[:-1]
+
+print(heightmap)
+
+current_height = heightmap[i][j]
+
+def Init():
+	for i in range(len(heightmap)):
+		for j in range(heightmap[i]):
+			neighbours = []
+			neighbours.append(heightmap[i + 1][j + 1])
+			neighbours.append(heightmap[i - 1][j + 1])
+			neighbours.append(heightmap[i + 1][j - 1])
+			neighbours.append(heightmap[i - 1][j - 1])
+
+			if(heightmap[i][j] == "S"):
+				current_height = heightmap[i][j]
+
+			
+def Step(heightmap, current_height, neighbour, count):
+	count += 1
+	current_height = neighbour
+	Step(heightmap, current_height)
+
+	for neighbour in neighbours:
+		if(ord(neighbour) - ord(current_height)):
+			Step(current_height, neighbours)
+
+
+
+
+
 
 # part 1: 
 print("day12: solution for part 1: " + str())
