51aeeace58c4181742db8ba71afe06643f2b3597
Simon <simol@student.chalmers.se>
2022-12-14 19:04:32 +0100

    day13 wip

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,5 +1,6 @@
 def Compare(val1, val2):
 	if(isinstance(val1, str) and isinstance(val2, str)):
+		#print(f"comparing {val1} to {val2}")
 		if(int(val1) > int(val2)):
 			return 1
 		elif(int(val1) < int(val2)):
@@ -7,22 +8,37 @@ def Compare(val1, val2):
 		else:
 			return -1
 	elif(isinstance(val1, list) and isinstance(val2, list)):
+		#print(f"both lists: {val1} and {val2}")
+		
 		if(len(val1) == 0 and len(val2) == 0):
+			print(f"both lengths zero for {val1}, {val2}")
 			return -1
 		elif(len(val1) == 0):
-			right_order.append(j)
-			return -1
+			return 2
 		elif(len(val2) == 0):
-			return -1
-		else:
-			return Compare(val1[0], val2[0])
+			return 1
+
+		for j in range(min(len(val2), len(val1))):
+			#print(f"sending with {val1[j], val2[j]}")
+			val = Compare(val1[j], val2[j])
+			if(val != -1):
+				#print(f"returning {val} for values: {val1[j]} and {val2[j]}")
+				return val
 	else:
 		if(isinstance(val1, str)):
 			val1 = list(val1)
 		elif(isinstance(val2, str)):
 			val2 = list(val2)
 
-		return Compare(val1, val2)
+		if(len(val1) == 0 and len(val2) == 0):
+			print(f"both lengths zero for {val1}, {val2}")
+			return -1
+		elif(len(val1) == 0):
+			return 2
+		elif(len(val2) == 0):
+			return 1
+
+		return Compare(val1[0], val2[0])
 
 def EntryList(i, packet, some_list):
 	local_list = []
@@ -72,19 +88,26 @@ while i := input.readline()[:-1]:
 
 	val = 12345
 	for (val1, val2) in zip(flat_result, flat_result2):
+		#print(f"init: {val1}, {val2}")
 		val = Compare(val1, val2)
+		
 		if(val == 1):
+			break
+		if(val == 2):
 			right_order.append(j)
-
+			break
+	print(val1)
 	if(val == -1):
-		if(len(flat_result) < len(flat_result2)):
+		if(len(val1) < len(val2)):
 			right_order.append(j)
+			break
 
 
 	#newline
 	i = input.readline()[:-1]
 	j += 1
 
+#between 5577 and 56xx
 # part 1: 
 print("day13: solution for part 1: " + str(sum(right_order)))
 
