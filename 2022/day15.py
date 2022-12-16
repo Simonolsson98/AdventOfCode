488731db8c4789eadd90d60aa494696a3dd6faa6
Simon <simol@student.chalmers.se>
2022-12-16 01:56:53 +0100

    day15

--- 2022/day15.py
+++ 2022/day15.py
@@ -2,7 +2,16 @@ input = open("day15_input.txt")
 i = input.readline()[:-1]
 
 while i:
-	
+	sensor = i[10:].split(", ")
+	sensor_x = sensor[0].split("=")[1]
+	sensor_y = sensor[1].split(":")[0].split("=")[1]
+
+	beacon = i.split("is at ")[1].split(",")
+	beacon_x = beacon[0].split("=")[1]
+	beacon_y = beacon[1].split("=")[1]
+
+	manhattan_dist = abs(int(sensor_x) - int(beacon_x)) + abs(int(sensor_y) - int(beacon_y))
+	print(f"manhattan_dist: {manhattan_dist}")
 	i = input.readline()[:-1]
 
 # part 1: 
