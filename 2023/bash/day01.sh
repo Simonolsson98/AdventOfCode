c172b10b57a683260fd8f589b3ba9598e1f0ec12
Simon Olsson <Simon.Olsson@csamhealth.com>
2023-12-01 17:33:13 +0100

    day 1 bash

---
+++ 2023/bash/day01.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+input="./day01_input.txt"
+
+starttime=$(date +"%s")
+number=0
+
+while IFS= read -r line
+do
+    length=${#line}
+    firstnum=""
+    lastnum=""
+    for ((i = 0; i < length+1; i++)); do
+        char="${line:i:1}"
+
+        if [[ "$char" =~ ^[0-9]+$ ]]; then
+            firstnum=$char
+            break
+        fi
+    done
+
+    for (( i = length - 2; i >= 0; i--)); do
+        char="${line:i:1}"
+        if [[ "$char" =~ ^[0-9]+$ ]]; then
+            lastnum=$char
+            break
+        fi
+    done
+
+    number=$(( number + $firstnum$lastnum ))
+done < "$input"
+
+echo $number
+echo "elapsed time in seconds: $(($(date +"%s")-$starttime))"
+
+starttime=$(date +"%s")
+number=0
+declare -A myArray=(["one"]="1" ["two"]="2" ["three"]="3" ["four"]="4" ["five"]="5" ["six"]="6" ["seven"]="7" ["eight"]="8" ["nine"]="9")
+while IFS= read -r line
+do
+    substr=""
+    substitutedline="$line"
+    length=${#line}
+    firstnum=""
+    lastnum=""
+    for ((i = 0; i < length+1; i++)); do
+        char="${line:i:1}"
+
+        if [[ "$char" =~ ^[0-9]+$ ]]; then
+            firstnum=$char
+            break
+        else
+            substr=$substr$char
+            for key in "${!myArray[@]}"; do
+                if [[ "$substr" == *"$key"* ]]; then
+                    firstnum=${myArray[$key]}
+                    break
+                fi
+            done
+            if [[ $firstnum != "" ]]; then
+                break
+            fi
+        fi
+    done
+
+    substr=""
+    for (( i = length - 2; i >= 0; i--)); do
+        char="${line:i:1}"
+        if [[ "$char" =~ ^[0-9]+$ ]]; then
+            lastnum=$char
+            break
+        else
+            substr=$char$substr
+            for key in "${!myArray[@]}"; do
+                if [[ "$substr" == *"$key"* ]]; then
+                    lastnum=${myArray[$key]}
+                    break
+                fi
+            done
+        fi
+        if [[ $lastnum != "" ]]; then
+            break
+        fi
+    done
+
+    number=$(( number + $firstnum$lastnum ))
+done < "$input"
+
+echo $number
+echo "elapsed time in seconds: $(($(date +"%s")-$starttime))"
+
+#not 50350
+#not 53518
+#not 53410
