21925c544a73172f4d84900e1d4839d3a498d407
Simon <simol@student.chalmers.se>
2023-12-04 19:59:10 +0100

    day4 python done

--- 2023/python/day04.py
+++ 2023/python/day04.py
@@ -2,49 +2,49 @@ import os
 
 def part(part_num):
     result = 0
+    part2res = 0
     numdict={"1": 1}
     with open(os.path.dirname(__file__)+"/day04_input.txt", 'r') as input_text:
-        maxlines=198
         for card, line in enumerate(input_text, 1):
             try:
-                number_of_iter = numdict[str(card)]
+                number_of_cards = numdict[str(card)] # iterate over copies if we have
             except:
-                number_of_iter = 1
+                number_of_cards = 1 # we only have original card
+            
+            numbersplits = line.split(':')[1].split(' | ')
+            winningnumbers=" ".join(numbersplits[0].split()).split(' ')
+            havenumbers=" ".join(numbersplits[1].split()).split(' ')
 
-            for _number_of_cards in range(number_of_iter):
-                numbersplits = line.split(':')[1].split(' | ')
-                winningnumbers=" ".join(numbersplits[0].split()).split(' ')
-                havenumbers=" ".join(numbersplits[1].split()).split(' ')
+            if(part_num == "1"):
+                for _ in range(number_of_cards):
+                    score=0
+                    for num in havenumbers:
+                        if num in winningnumbers:
+                            if score == 0:
+                                score = 1
+                            else:
+                                score *= 2
+                    result += score
 
-                score=0
+            if(part_num == "2"):
+                part2res += number_of_cards # save cards
                 numberofwinningcards=0
                 for num in havenumbers:
                     if num in winningnumbers:
                         numberofwinningcards += 1
-                        if score == 0:
-                            score = 1
-                        else:
-                            score *= 2
-                result += score
 
-                if(numberofwinningcards == 0 and str(card) not in numdict):
-                    print(f"NOT IN {str(card)}")
-                    numdict.update( {str(card): 1 } )
-                    break
-
-                #print(f"numberofwinningcards: {numberofwinningcards} for {card}")
                 for index in range(1, numberofwinningcards + 1):
-                    if card + index <= maxlines + 1:
+                    maxlines=198
+                    if card + index <= maxlines: # not really needed since it stops at the end of the input data anyways
                         try: 
-                            numdict.update( {str(card + index): numdict[str(card + index)] + 1} )
+                            numdict.update( {str(card + index): numdict[str(card + index)] + number_of_cards} ) #
                         except KeyError: 
-                            numdict.update( {str(card + index): 2 } )
+                            numdict.update( {str(card + index): number_of_cards + 1 } ) # save copies + original card
 
     if part_num == "1":
-        print(f"day2: Python solution for part 1: {result}")
+        print(f"day4: Python solution for part 1: {result}")
     elif part_num == "2":
-        print(f"day2: Python solution for part 2: {sum(numdict.values())}")
+        print(f"day4: Python solution for part 2: {part2res}")
 
-#part("1")
+part("1")
 part("2")
-# too low: 8477777
