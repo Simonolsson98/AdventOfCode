e2e2de1add7caa7f54b0d3054df080ed01d515a4
Simon Olsson <Simon.Olsson@csamhealth.com>
2023-12-07 11:01:43 +0100

    day07 python part2 WP

--- 2023/day07.py
+++ 2023/day07.py
@@ -3,38 +3,83 @@ import os
 def part(part_num):
     possible_cards = [str(i) for i in range(9, 1, -1)]
     values = [(i, []) for i in range(1,8)]
-    possible_cards=["A", "K", "Q", "J", "T"] + possible_cards
-    print(possible_cards)
+    if(part_num == "1"):
+        possible_cards=["A", "K", "Q", "J", "T"] + possible_cards
+    else:
+        possible_cards=["A", "K", "Q", "T"] + possible_cards + ["J"]
     with open(os.path.dirname(__file__)+"/day07_input.txt", 'r') as input_text:
         while asd := input_text.readline():
-            
             cards, bet = asd.split(" ")
+            
             occurrences = []
             for i in possible_cards:
+                if(cards.count(i) == 0):
+                    continue
                 occurrences.append(cards.count(i))
 
-            new_order = ""
-            for (card, count) in [(card, count) for (card, count) in zip(possible_cards, occurrences) if occurrences != 0]:
-                new_order += card*count
-
-            zeroes=occurrences.count(0)
-            for i in range(zeroes):
-                occurrences.remove(0)
-
             if(occurrences == [5]):
-                values[6][1].append((cards, bet.rstrip()))
+                if(part_num == "2" and (jokers := cards.count("J")) > 0):
+                    values[6][1].append(("AAAAA", bet.rstrip()))
+                else:
+                    values[6][1].append((cards, bet.rstrip()))
+
             elif(occurrences == [1, 4] or occurrences == [4, 1]):
-                values[5][1].append((cards, bet.rstrip()))
+                if(part_num == "2" and (jokers := cards.count("J")) > 0):
+                    if(jokers == 1):
+                        specific_card = [card for card in cards if card != 'J']
+                        cards = "".join(specific_card[0]*5)
+                    else: #jokers == 4
+                        specific_card = [card for card in cards if card != 'J']
+                        cards = "".join(specific_card*5)
+                else:
+                    values[5][1].append((cards, bet.rstrip()))
+
             elif(occurrences == [2, 3] or occurrences == [3, 2]):
-                values[4][1].append((cards, bet.rstrip()))
-            elif(sorted(occurrences) == sorted([2, 3]) or sorted(occurrences) == sorted([1, 1, 3])):
-                values[3][1].append((cards, bet.rstrip()))
+                if(part_num == "2" and (jokers := cards.count("J")) > 0):
+                    if (jokers != 3):
+                        specific_card = [card for card in cards if cards.count(card) == 3]
+                        cards = cards.replace('J', specific_card[0])
+                    elif (jokers == 3):
+                        specific_card = [card for card in cards if cards.count(card) != 3]
+                        cards = cards.replace('J', specific_card[0])
+                else:
+                    values[4][1].append((cards, bet.rstrip()))
+
+            elif(sorted(occurrences) == sorted([1, 1, 3])):
+                if(part_num == "2" and (jokers := cards.count("J")) > 0):
+                    if(jokers == 1 or jokers == 2):
+                        specific_card = [card for card in cards if cards.count(card) == 3]
+                        cards = cards.replace('J', specific_card[0])
+                    if(jokers == 3): # none of this in input data so idc
+                        pass
+                else:
+                    values[3][1].append((cards, bet.rstrip()))
+
             elif(sorted(occurrences) == sorted([2, 2, 1])):
-                values[2][1].append((cards, bet.rstrip()))
+                if(part_num == "2" and (jokers := cards.count("J")) > 0):
+                    if(jokers == 1):
+
+                    elif (jokers == 2):
+                        print(cards)
+                        specific_card = [card for card in cards if cards.count(card) == 2 and card != 'J']
+                        print(specific_card)
+                        cards = cards.replace('J', specific_card[0])
+                        print(cards)
+                else:
+                    values[2][1].append((cards, bet.rstrip()))
+
             elif(sorted(occurrences) == sorted([2, 1, 1, 1])):
-                values[1][1].append((cards, bet.rstrip()))
+                if(part_num == "2" and (jokers := cards.count("J")) > 0):
+                    pass
+                else:
+                    values[1][1].append((cards, bet.rstrip()))
+
             elif(occurrences == [1, 1, 1, 1, 1]):
-                values[0][1].append((cards, bet.rstrip()))
+                if(part_num == "2" and (jokers := cards.count("J")) > 0):
+                    pass
+                else:
+                    values[0][1].append((cards, bet.rstrip()))
+
 
         rank = 1
         result = []
@@ -49,7 +94,7 @@ def part(part_num):
     if part_num == "1":
         print(f"day7: Python solution for part 1: {sum(result)}")
     elif part_num == "2":
-        print(f"day7: Python solution for part 2: {values}")
+        print(f"day7: Python solution for part 2: {sum(result)}")
 
 part("1")
-#part("2")
+part("2")
