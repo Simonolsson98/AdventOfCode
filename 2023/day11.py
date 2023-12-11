aa866cce1c6210b88e4c0aa384b81a145ab9508a
Simon Olsson <Simon.Olsson@csamhealth.com>
2023-12-11 16:08:58 +0100

    day11 python all done

--- 2023/day11.py
+++ 2023/day11.py
@@ -4,56 +4,54 @@ import time
 
 def part(part_num):
     arr=[]
-    galaxies=[]
-    empty_rows = []
-    empty_cols = []
     with open(os.path.dirname(__file__)+"/day11_input.txt", 'r') as input_text:
+        empty_rows = []
         curr_index = 0
         for index, line in enumerate(input_text):
             arr.append(line.rstrip("\n"))
-            if "#" in line:
-                pass
-            else:
-                empty_rows.append(index)
-                arr[curr_index:curr_index + 1] = ["." * len(arr[0])] + ["." * len(arr[0])]
-                curr_index += 1
+            if "#" not in line:
+                if(part_num == "1"):
+                    arr[curr_index:curr_index + 1] = ["." * len(arr[0])] + ["." * len(arr[0])]
+                    curr_index += 1
+                else:
+                    empty_rows.append(index)
             curr_index += 1
 
     transposed = [''.join(list(x)) for x in zip(*arr)]
     index = 0
-    added = 0
+    empty_cols = []
     while index < len(transposed):
         row = transposed[index]
-        if "#" in row:
-            pass
-        else:
-            empty_cols.append(index - added)
-            transposed[index:index + 1] = ["." * len(transposed[0])] + ["." * len(transposed[0])]
-            index += 1
-            added += 1
-        
+        if "#" not in row:
+            if(part_num == "1"):
+                # part 1
+                transposed[index:index + 1] = ["." * len(transposed[0])] + ["." * len(transposed[0])]
+                index += 1
+            else:
+                # part 2
+                empty_cols.append(index)
         index += 1
 
-    test = [''.join(list(x)) for x in zip(*transposed)]
-    for i, row in enumerate(test):
+    galaxies=[]
+    transposed_back = [''.join(list(x)) for x in zip(*transposed)]
+    for i, row in enumerate(transposed_back):
         if "#" in row:
             indices = [i for i, x in enumerate(row) if x == "#"]
             for index in indices:
                 galaxies.append((i, index))
 
-    done = []
+    visited = []
     result = 0
-    i = 0
     for elem in galaxies:
         for elem_iter in galaxies:
-            if elem_iter != elem and elem_iter not in done:
+            if elem_iter != elem and elem_iter not in visited:
                 if(part_num == "1"):
+                    # part 1
                     result += abs(elem[0] - elem_iter[0]) + abs(elem[1] - elem_iter[1])
                 else:
+                    # part 2
                     rowval = 0
                     colval = 0
-                    print(empty_rows)
-                    print(empty_cols)
                     for emptyrow in empty_rows:
                         if (elem[0] > emptyrow and elem_iter[0] < emptyrow) or (elem[0] < emptyrow and elem_iter[0] > emptyrow):
                             rowval += 999999
@@ -61,17 +59,15 @@ def part(part_num):
                         if (elem[1] > emptycol and elem_iter[1] < emptycol) or (elem[1] < emptycol and elem_iter[1] > emptycol):
                             colval += 999999
 
-                    print(f"rowval: {rowval}, colval: {colval} for elem: {elem} + elem_iter: {elem_iter}")
-                    result += abs(elem[0] - elem_iter[0]) + rowval - 1 + abs(elem[1] - elem_iter[1]) + colval - 1
-        done.append(elem)
+                    result += abs(elem[0] - elem_iter[0]) + rowval + abs(elem[1] - elem_iter[1]) + colval
+        visited.append(elem)
 
     if part_num == "1":
-        print(f"day11: Python solution for part 1: {result}, time: {round(time.time() - start, 2)} s")
+        print(f"day11: Python solution for part 1: {result}, time: {round(time.time() - start, 3)} s")
     elif part_num == "2":
-        print(f"day11: Python solution for part 2: {result - len(arr[0])}, time: {round(time.time() - start, 2)} s")
+        print(f"day11: Python solution for part 2: {result}, time: {round(time.time() - start, 3)} s")
 
 start = time.time()
 part("1")
 start = time.time()
 part("2")
-# too high 416944935244
