b0494f51ab718758e8358b4612c32f4a1a3b63e6
Simon <simol@student.chalmers.se>
2023-12-18 17:38:25 +0100

    day18 part 1 60 sec runtime gg

--- 2023/day17.py
+++ 2023/day17.py
@@ -1,6 +1,7 @@
 import os
 import math
 import time
+import copy
 
 def part(part_num):
     with open(os.path.dirname(__file__)+"/day17_input.txt", 'r') as input_text:
@@ -15,51 +16,59 @@ def part(part_num):
             visited.append([not elem for elem in list(map(bool, str(line.strip())))])
             values.append([999] * len(line.rstrip("\n")))
 
-        curr_i = 0
-        curr_j = 0
         istraightcounter = 0
         jstraightcounter = 0
         values[0][0] = 0
-        while (not visited[len(visited) - 1][len(visited) - 1]):
-            visit_neighbour(curr_i, curr_j, istraightcounter, jstraightcounter, "right")
+        tempvisited = copy.deepcopy(visited)
+        tempvalues = copy.deepcopy(values)
+        for x in range(4):
+            for y in range(4):
+                curr_i = 0
+                curr_j = 0
+                istraightcounter = 0
+                jstraightcounter = 0
+
+                print(values)
+                while (not visited[len(visited) - 1][len(visited) - 1]):
+                    print(values)
+                    visit_neighbour(curr_i + x, curr_j + y, istraightcounter, jstraightcounter, "right")
+
+                visited = tempvisited
+                values = tempvalues
+
+                print(f"afteR: {values[len(values) - 1][len(values) - 1]}")
+                print(f"afteR: {visited}")
 
     if(part_num == "1"):
-        [print(i) for i in values]
         return values[len(visited) - 1][len(visited) - 1]
     else:
         pass
 
 def visit_neighbour(i: int, j: int, istraightcounter: int, jstraightcounter: int, direc: str):
-    if(i == 1 and j == 5):
-        print(f"UP: {values[i - 1][j]}, LEFT: {values[i][j - 1]}, RIGHT: {values[i][j + 1]}, DOWN: {values[i + 1][j]}")
-
-    if(direc != "down" and i - 1 >= 0 and istraightcounter < 3):
+    if(direc != "down" and i - 1 >= 0 and istraightcounter < 4):
         up = edges[i - 1][j]
         if(values[i][j] + up < values[i - 1][j]):
             values[i - 1][j] = values[i][j] + up
             visit_neighbour(i - 1, j, istraightcounter + 1, 0, "up")
 
-    if(direc != "right" and j - 1 >= 0 and jstraightcounter < 3):
+    if(direc != "right" and j - 1 >= 0 and jstraightcounter < 4):
         left = edges[i][j - 1]
         if(values[i][j] + left < values[i][j - 1]):
             values[i][j - 1] = values[i][j] + left
             visit_neighbour(i, j - 1, 0, jstraightcounter + 1, "left")
 
-    if(direc != "left" and j + 1 < len(visited[0]) and jstraightcounter < 3):
+    if(direc != "left" and j + 1 < len(visited[0]) and jstraightcounter < 4):
         right = edges[i][j + 1]
         if(values[i][j] + right < values[i][j + 1]):
             values[i][j + 1] = values[i][j] + right
             visit_neighbour(i, j + 1, 0, jstraightcounter + 1, "right")
 
-    if(direc != "up" and i + 1 < len(visited) and istraightcounter < 3):
+    if(direc != "up" and i + 1 < len(visited) and istraightcounter < 4):
         down = edges[i + 1][j]
         if(values[i][j] + down < values[i + 1][j]):
             values[i + 1][j] = values[i][j] + down
             visit_neighbour(i + 1, j, istraightcounter + 1, 0, "down")
-   
-    if(i == 1 and j == 5):
-        print(f"AFTER:::: UP: {values[i - 1][j]}, LEFT: {values[i][j - 1]}, RIGHT: {values[i][j + 1]}, DOWN: {values[i + 1][j]}")
-
+            
     visited[i][j] = True
     return
 
