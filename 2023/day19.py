e81b7d7d861629c1ba94ac8fab60e8053a69f124
Simon Olsson <Simon.Olsson@csamhealth.com>
2023-12-19 16:28:27 +0100

    day19 WIP part 2

--- 2023/day19.py
+++ 2023/day19.py
@@ -6,15 +6,20 @@ result = 0
 def part(part_num):
     with open(os.path.dirname(__file__)+"/day19_input.txt", 'r') as input_text:
         line = input_text.readline().rstrip()
+        global workflowdict
         workflowdict = {}
         while line != "":
             removebraces = line[:-1].split("{")
             workflowdict[removebraces[0]] = removebraces[1].split(",")
             line = input_text.readline().rstrip()
 
-        if(part_num == "1"):
+        if part_num == "2":
+            currentdict = workflowdict["in"]
+            checkworkflow(currentdict, [])
+
+        elif(part_num == "1"):
             parts = input_text.readline().rstrip()
-            winning = []
+            winning = [(0, 0), (0, 0), (0, 0), (0, 0)]
             while parts != "":
                 removebraces = "".join(parts[:-1].split("{"))
                 values = removebraces.split(",")
@@ -64,9 +69,49 @@ def part(part_num):
 
                 parts = input_text.readline().rstrip()
 
-        return sum(winning)
+            return sum(winning)
 
+def checkworkflow(workflowline, winning):
+    for workflow in workflowline:
+        if('<' in workflow or '>' in workflow or '=' in workflow):
+            split = workflow.split(":")
+            newrange = split[0][1:]
+            if(split[1] == "A")
+                if(split[0][0] == 'x'):
+                    if '<' in split[0] and newrange < winning[0][1]:
+                        winning[0][1] = newrange
+                    elif '>' in split[0] and newrange > winning[0][0]:
+                        winning[0][0] = newrange:
+                    else: #"=" in split[0] 
+                        pass
+                elif(split[0][0] == 'm'):
+                    if '<' in split[0] and newrange < winning[1][1]:
+                        winning[1][1] = newrange
+                    elif '>' in split[0] and newrange > winning[1][0]:
+                        winning[1][0] = newrange:
+                    else: #"=" in split[0] 
+                        pass
+                elif(split[0][0] == 'a'):
+                    if '<' in split[0] and newrange < winning[2][1]:
+                        winning[2][1] = newrange
+                    elif '>' in split[0] and newrange > winning[2][0]:
+                        winning[2][0] = newrange:
+                    else: #"=" in split[0] 
+                        pass
+                elif(split[0][0] == 's'):
+                    if '<' in split[0] and newrange < winning[3][1]:
+                        winning[3][1] = newrange
+                    elif '>' in split[0] and newrange > winning[3][0]:
+                        winning[3][0] = newrange:
+                    else: #"=" in split[0] 
+                        pass
+            elif(split[1] == "R")
+                pass
+            else:
+                checkworkflow(workflowdict[split[1]])
+
+
+#start = time.time()
+#print(f"day19: Python solution for part 1: {part('1')}, time: {round(time.time() - start, 5)} s")
 start = time.time()
-print(f"day19: Python solution for part 1: {part('1')}, time: {round(time.time() - start, 5)} s")
-start = time.time()
-#print(f"day19: Python solution for part 2: {part('2')}, time: {round(time.time() - start, 5)} s")
+print(f"day19: Python solution for part 2: {part('2')}, time: {round(time.time() - start, 5)} s")
