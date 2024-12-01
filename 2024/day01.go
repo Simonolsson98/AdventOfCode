9b2caa2086c71b1944f7d1813b92fee57894a3f4
Simon <simol@student.chalmers.se>
2024-12-01 13:53:12 +0100

    day1 both parts

--- 2024/day01.go
+++ 2024/day01.go
@@ -2,15 +2,76 @@ package main
 
 import (
     "fmt"
-    "log"
-    "github.com/Simonolsson98/AdventOfCode/tree/main/2024/Utils/readinput"  // Import the utils package
+    "2024/utils"
+    "os"
+    "path/filepath"
+    "sort"
+    "strconv"
+    "strings"
 )
 
 func main() {
-    input, err := utils.ReadInput("day01_input")  // Call ReadInput from utils
+    inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
+    input, err := utils.ReadInput(inputFile)
     if err != nil {
-        log.Fatalf("Error reading input: %v", err)
+        fmt.Println("Error reading input:", err)
+        return
     }
-    fmt.Println("Day 1 Solution:")
-    // Solution logic here...
+
+    list1, list2, err := parseInput(input)
+    if err != nil {
+        fmt.Println("Error parsing input:", err)
+        return
+    }
+
+    // Solve parts
+    fmt.Println("Day 1 Solution (Part 1):", part1(list1, list2))
+    fmt.Println("Day 1 Solution (Part 2):", part2(list1, list2))
+}
+
+func parseInput(input string) ([]int, []int, error) {
+    list1 := []int{}
+    list2 := []int{}
+    for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+        fields := strings.Fields(line)
+        if len(fields) < 2 {
+            return nil, nil, fmt.Errorf("invalid input line: %s", line)
+        }
+        fst, err1 := strconv.Atoi(fields[0])
+        snd, err2 := strconv.Atoi(fields[1])
+        if err1 != nil || err2 != nil {
+            return nil, nil, fmt.Errorf("failed to parse integers: %v, %v", err1, err2)
+        }
+        list1 = append(list1, fst)
+        list2 = append(list2, snd)
+    }
+    return list1, list2, nil
+}
+
+func part1(list1, list2 []int) int {
+    sort.Ints(list1)
+    sort.Ints(list2)
+
+    diff := 0
+    for i := 0; i < len(list1); i++ {
+        res := list1[i] - list2[i]
+        if res < 0 {
+            res = -res
+        }
+        diff += res
+    }
+    return diff
+}
+
+func part2(list1, list2 []int) int {
+    counter := map[int]int{} // Use a map for counting occurrences in list2
+    for _, val := range list2 {
+        counter[val]++
+    }
+
+    total := 0
+    for _, val := range list1 {
+        total += counter[val] * val
+    }
+    return total
 }
