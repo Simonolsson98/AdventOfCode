f699ebb4fd8254bd6385e5ac4e9a839396b384f5
Simon <simol@student.chalmers.se>
2024-12-01 13:55:10 +0100

    day2 init

---
+++ 2024/day02.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
+    input, err := utils.ReadInput(inputFile)
+    if err != nil {
+        fmt.Println("Error reading input:", err)
+        return
+    }
+}
