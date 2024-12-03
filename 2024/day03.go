d37a26cfe8cc9deba0d517ceb1568aabff53f908
Simon Olsson <Simon.Olsson@omda.com>
2024-12-03 07:56:13 +0100

    day3 part2 done

--- 2024/day03.go
+++ 2024/day03.go
@@ -19,9 +19,10 @@ func main() {
     }
 
     sum := part1(input)
+    sum2 := part2(input)
 
     fmt.Println("Day 3 Solution (Part 1):", sum)
-    fmt.Println("Day 3 Solution (Part 2):")
+    fmt.Println("Day 3 Solution (Part 2):", sum2)
 }
 
 func part1(input string) (result int) {
@@ -39,6 +40,39 @@ func part1(input string) (result int) {
             snd, _ := strconv.Atoi(val[1])
 
             sum += (fst * snd)
+        }
+    }
+
+    return sum
+}
+
+func part2(input string) (result int) {
+    re, err := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)|do(?:n't)?\(\)`)
+    if err != nil {
+        fmt.Println("wtf")
+        return
+    }
+
+    var sum int;
+    fmt.Println(re.FindAllString(input, -1))
+    var include bool = true;
+    for _, match := range re.FindAllString(input, -1) {
+        if match == "do()" {
+            include = true
+            continue
+        } else if match == "don't()" {
+            include = false
+            continue
+        }
+
+        if include {
+            for _, nums := range regexp.MustCompile(`\d{1,3},\d{1,3}`).FindAllString(match, -1) {
+                val := strings.Split(nums, ",")
+                fst, _ := strconv.Atoi(val[0])
+                snd, _ := strconv.Atoi(val[1])
+
+                sum += (fst * snd)
+            }
         }
     }
 
