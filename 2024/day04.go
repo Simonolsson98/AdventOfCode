ddeb889a7a452dd5eb9bb845e8b402a04d5e41ce
Simon Olsson <Simon.Olsson@omda.com>
2024-12-04 09:33:06 +0100

    day4 part1 done

--- 2024/day04.go
+++ 2024/day04.go
@@ -17,6 +17,80 @@ func main() {
         return
     }
 
-    fmt.Println("Day 4 Solution (Part 1):")
+    var grid [][]rune
+    for _, line := range strings.Split(input, "\n") {
+        grid = append(grid, []rune(line))
+    }
+
+    var sum int
+    for i, row := range grid {
+        for j, char := range row {
+            fmt.Printf("Character at [%d][%d]: %c\n", i, j, char)
+            sum += CheckPart1Matches(char, grid, i, j)
+        }
+    }
+
+    fmt.Println("Day 4 Solution (Part 1):", sum)
     fmt.Println("Day 4 Solution (Part 2):")
+}
+
+func CheckPart1Matches(character rune, grid [][]rune, i int, j int) (sum int){
+    // Down
+    if i+3 < len(grid) && character == 'X' && 
+        grid[i+1][j] == 'M' && 
+        grid[i+2][j] == 'A' && 
+        grid[i+3][j] == 'S' { 
+        sum += 1
+    }
+    // Up
+    if i-3 >= 0 && character == 'X' && 
+       grid[i-1][j] == 'M' && 
+       grid[i-2][j] == 'A' && 
+       grid[i-3][j] == 'S' { 
+        sum += 1
+    }
+    // Right
+    if j+3 < len(grid[0]) && character == 'X' && 
+       grid[i][j+1] == 'M' && 
+       grid[i][j+2] == 'A' && 
+       grid[i][j+3] == 'S' { 
+        sum += 1
+    }
+    // Left
+    if j-3 >= 0 && character == 'X' && 
+       grid[i][j-1] == 'M' && 
+       grid[i][j-2] == 'A' && 
+       grid[i][j-3] == 'S' { 
+        sum += 1
+    }
+    // Diagonal Down-Right
+    if i+3 < len(grid) && j+3 < len(grid[0]) && character == 'X' && 
+       grid[i+1][j+1] == 'M' && 
+       grid[i+2][j+2] == 'A' && 
+       grid[i+3][j+3] == 'S' {
+        sum += 1
+    }
+    // Diagonal Down-Left
+    if i+3 < len(grid) && j-3 >= 0 && character == 'X' && 
+       grid[i+1][j-1] == 'M' && 
+       grid[i+2][j-2] == 'A' && 
+       grid[i+3][j-3] == 'S' { 
+        sum += 1
+    }
+    // Diagonal Up-Right
+    if i-3 >= 0 && j+3 < len(grid[0]) && character == 'X' && 
+       grid[i-1][j+1] == 'M' && 
+       grid[i-2][j+2] == 'A' && 
+       grid[i-3][j+3] == 'S' { 
+        sum += 1
+    }
+    // Diagonal Up-Left
+    if i-3 >= 0 && j-3 >= 0 && character == 'X' && 
+       grid[i-1][j-1] == 'M' && 
+       grid[i-2][j-2] == 'A' && 
+       grid[i-3][j-3] == 'S' { 
+        sum += 1
+    }
+
+    return sum
 }
