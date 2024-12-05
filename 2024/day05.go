6ddaeb3bd0a5b0696ca5f11bdb1f481cd991fd9e
Simon <simol@student.chalmers.se>
2024-12-05 17:21:12 +0100

    somehow part1 works

--- 2024/day05.go
+++ 2024/day05.go
@@ -3,10 +3,10 @@ package main
 import (
     "fmt"
     "strings"
-    "os"
-    "2024/utils"
     "strconv"
+    "os"
     "path/filepath"
+    "2024/utils"
 )
 
 func main() {
@@ -22,126 +22,73 @@ func main() {
     rules := strings.Split(parts[0], "\n")
     inputRows := strings.Split(parts[1], "\n")
 
-    // Create root node
-    var root *TreeNode = &TreeNode{Value: -1}
+    // Create a list to store the rules as pairs
+    constraints := make([][2]int, 0)
+
+    // Parse the rules into the constraints list
     for _, rule := range rules {
         res := strings.Split(rule, "|")
         firstNum, _ := strconv.Atoi(res[0])
         secondNum, _ := strconv.Atoi(res[1])
-
-        // Find all nodes with the value of firstNum
-        foundNodes := root.FindAllNodes(firstNum)
-
-        if len(foundNodes) == 0 {
-            // If no node is found with firstNum, create a new root node and add the child
-            newRoot := &TreeNode{Value: firstNum}
-            child := &TreeNode{Value: secondNum}
-            newRoot.AddChild(child)
-            root.AddChild(newRoot)
-        } else {
-            // Add the secondNum as a child of all found nodes with value firstNum
-            for _, foundNode := range foundNodes {
-                if secondNum == 53 {
-                    fmt.Println("ADDING 53 to this: ", foundNode.Value)
-                }
-                child := &TreeNode{Value: secondNum}
-                foundNode.AddChild(child)
-            }
-        }
+        constraints = append(constraints, [2]int{firstNum, secondNum})
     }
 
-    var asd []string
-    for _, inputRow := range inputRows {
-        ok := true
-        currNode := root
-        nums := strings.Split(inputRow, ",")
-        for _, num := range nums {
-            realNum, _ := strconv.Atoi(num)
-            fmt.Println("looking for node with val: ", realNum, "looking from node:", currNode.Value)
-            nextNode := currNode.FindNode(realNum)
-            if nextNode == nil {
-                ok = false
-                fmt.Println("didn't find node: ", realNum)
-                break
-            }
+    sum := part1(inputRows, constraints)
 
-            currNode = nextNode
-        }
-
-        if ok {
-            fmt.Println("ok")
-            asd = append(asd, inputRow)
-        }
-    }
-
-    // Print the tree structure
-    root.PrintTree("")
-
-    fmt.Println("Day 5 Solution (Part 1):")
+    fmt.Println("Day 5 Solution (Part 1):", sum)
     fmt.Println("Day 5 Solution (Part 2):")
 }
 
-// TreeNode represents a single node in the tree
-type TreeNode struct {
-    Value    int          // The value of the node
-    Children []*TreeNode  // Slice of child nodes
-}
+func part1(inputRows []string, constraints [][2]int) (int) {
+    var validPaths []string
+    var invalidPaths []string
+    var sum int
 
-func (n *TreeNode) AddChild(child *TreeNode) {
-    n.Children = append(n.Children, child)
-}
+    for _, inputRow := range inputRows {
+        nums := strings.Split(inputRow, ",")
+        valid := true
 
-func (n *TreeNode) PrintTree(indent string) {
-    fmt.Println(indent + strconv.Itoa(n.Value))
-    for _, child := range n.Children {
-        child.PrintTree(indent + "    ")
-    }
-}
+        // Check each constraint
+        for _, constraint := range constraints {
+            firstNum, secondNum := constraint[0], constraint[1]
 
-// FindNode finds the first node in the tree with the specified value using BFS
-func (n *TreeNode) FindNode(value int) *TreeNode {
-    // Create a queue for BFS
-    queue := []*TreeNode{n}
+            firstIndex := findIndex(nums, firstNum)
+            secondIndex := findIndex(nums, secondNum)
 
-    // Process nodes in the queue
-    for len(queue) > 0 {
-        // Dequeue the first node
-        current := queue[0]
-        queue = queue[1:]
+            // Valid if either constraint is not part of it, or first element appears before second element
+            if (firstIndex == -1 || secondIndex == -1) || firstIndex < secondIndex {
+                continue
+            }
 
-        // Check if the current node matches the value
-        if current.Value == value {
-            return current
+            valid = false
+            break
         }
 
-        // Enqueue all the children of the current node
-        queue = append(queue, current.Children...)
+        // Append to valid or invalid paths
+        if valid {
+            validPaths = append(validPaths, inputRow)
+        } else {
+            invalidPaths = append(invalidPaths, inputRow)
+        }
     }
 
-    // Return nil if the value is not found
-    return nil
+    // Calculate the sum of middle elements for valid paths
+    for _, validPath := range validPaths {
+        epiclist := strings.Split(validPath, ",")
+        ffs, _ := strconv.Atoi(epiclist[(len(epiclist)-1)/2])
+        sum += ffs
+    }
+
+    return sum
 }
 
-// FindAllNodes finds all nodes in the tree with the specified value using BFS
-func (n *TreeNode) FindAllNodes(value int) []*TreeNode {
-    var result []*TreeNode
-    queue := []*TreeNode{n}
-
-    // Process nodes in the queue
-    for len(queue) > 0 {
-        // Dequeue the first node
-        current := queue[0]
-        queue = queue[1:]
-
-        // Check if the current node matches the value
-        if current.Value == value {
-            result = append(result, current)
+// Helper function to find the index of a number in a slice
+func findIndex(nums []string, target int) int {
+    for i, num := range nums {
+        realNum, _ := strconv.Atoi(num)
+        if realNum == target {
+            return i
         }
-
-        // Enqueue all the children of the current node
-        queue = append(queue, current.Children...)
     }
-
-    // Return the list of matching nodes
-    return result
+    return -1
 }
