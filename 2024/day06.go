9f9dd02593283df1afeffe82cc8a4eb1b2aa1fc2
Simon Olsson <Simon.Olsson@omda.com>
2024-12-06 10:29:50 +0100

    day 6 part 2 working but über slow

--- 2024/day06.go
+++ 2024/day06.go
@@ -12,13 +12,22 @@ import (
 type someStruct struct  {
     posX int
     posY int
+    dir int
 }
 func (s someStruct) key() string {
     return strconv.Itoa(s.posX) + "," + strconv.Itoa(s.posY)
 }
+func (s someStruct) key2() string {
+    return strconv.Itoa(s.posX) + "," + strconv.Itoa(s.posY) + "," + strconv.Itoa(s.dir)
+}
 
 var visited []someStruct
 var uniqueMap map[string]bool = make(map[string]bool)
+var uniqueVisited []someStruct
+var grid [][]string
+
+var startingPosX int
+var startingPosY int
 
 func main() {
     inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
@@ -28,8 +37,6 @@ func main() {
         return
     }
 
-    
-    var grid [][]string
     var currPosX int
     var currPosY int
     for x, line := range strings.Split(input, "\n") {
@@ -39,16 +46,16 @@ func main() {
             if char == "^" {
                 currPosX = x
                 currPosY = y
+
+                startingPosX = currPosX
+                startingPosY = currPosY
             }
         }
 
         grid = append(grid, col)
     }
 
-
     part1(grid, currPosX, currPosY)
-
-    fmt.Println("Day 6 Solution (Part 2):")
 }
 
 func part1(grid [][]string, startX, startY int) {
@@ -100,7 +107,6 @@ func part1(grid [][]string, startX, startY int) {
 
 func GuardOutOfSight(){
     if r := recover(); r != nil {
-        uniqueVisited := []someStruct {}
         for _, pos := range visited {
             if !uniqueMap[pos.key()] {
                 uniqueVisited = append(uniqueVisited, pos)
@@ -110,4 +116,110 @@ func GuardOutOfSight(){
 
         fmt.Println("Day 6 Solution (Part 1):", len(uniqueVisited))
     }
+
+    var differentPositions int
+    for x, line := range grid {
+        for y, _ := range line {
+            uniqueMap = make(map[string]bool)
+            if grid[x][y] == "." {
+                grid[x][y] = "#"
+            } else{
+                continue
+            }
+
+            hmm := part2(grid)
+            if hmm {
+                differentPositions += 1
+            }
+
+            grid[x][y] = "."
+        }
+    }
+
+    fmt.Println("Day 6 Solution (Part 2):", differentPositions)
+}
+
+func part2(grid [][]string) (loop bool) {
+    currPosX, currPosY := startingPosX, startingPosY
+    direction := 0
+
+    // Add the starting position
+    visited = append(visited, someStruct{posX: currPosX, posY: currPosY, dir: direction})
+
+    for ok := true; ok; {
+        var nextEle string
+        switch direction {
+        case 0: // Up
+            if currPosX - 1 < 0 {
+                return false
+            }
+            nextEle = grid[currPosX-1][currPosY]
+            if nextEle == "#" {
+                direction = (direction + 1) % 4
+            } else {
+                currPosX--
+                pos := someStruct{posX: currPosX, posY: currPosY, dir: direction}
+                if !uniqueMap[pos.key2()] {
+                    uniqueVisited = append(uniqueVisited, pos)
+                    uniqueMap[pos.key2()] = true
+                } else {
+                    return true
+                }
+            }
+        case 1: // Right
+            if currPosY + 1 > len(grid[0]) - 1 {
+                return false
+            }
+            nextEle = grid[currPosX][currPosY+1]
+            if nextEle == "#" {
+                direction = (direction + 1) % 4
+            } else {
+                currPosY++
+                pos := someStruct{posX: currPosX, posY: currPosY, dir: direction}
+                if !uniqueMap[pos.key2()] {
+                    uniqueVisited = append(uniqueVisited, pos)
+                    uniqueMap[pos.key2()] = true
+                } else {
+                    return true
+                }
+            }
+        case 2: // Down
+            if currPosX + 1 > len(grid) - 1 {
+                return false
+            }
+            nextEle = grid[currPosX+1][currPosY]
+            if nextEle == "#" {
+                direction = (direction + 1) % 4
+            } else {
+                currPosX++
+                pos := someStruct{posX: currPosX, posY: currPosY, dir: direction}
+                if !uniqueMap[pos.key2()] {
+                    uniqueVisited = append(uniqueVisited, pos)
+                    uniqueMap[pos.key2()] = true
+                } else {
+                    return true
+                }
+            }
+        case 3: // Left
+            if currPosY - 1 < 0 {
+                return false
+            }
+            nextEle = grid[currPosX][currPosY-1]
+            if nextEle == "#" {
+                direction = (direction + 1) % 4
+            } else {
+                currPosY--
+                pos := someStruct{posX: currPosX, posY: currPosY, dir: direction}
+                if !uniqueMap[pos.key2()] {
+                    uniqueVisited = append(uniqueVisited, pos)
+                    uniqueMap[pos.key2()] = true
+                } else {
+                    return true
+                }
+            }
+        }
+    }
+
+    // wont reach
+    return false
 }
