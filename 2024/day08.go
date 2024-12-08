3ac08df38f9e4d9ba7410b9e8f2117420e008722
Simon <simol@student.chalmers.se>
2024-12-08 13:59:57 +0100

    day8 part1 done

--- 2024/day08.go
+++ 2024/day08.go
@@ -19,8 +19,7 @@ func main() {
     }
 
 	start := time.Now()
-	// exec part1()
-    fmt.Println("Day 8 Solution (Part 1):")
+    fmt.Println("Day 8 Solution (Part 1):", part1(input))
     fmt.Println("Part 1 execution time:", time.Since(start), "\n")
 
 	start = time.Now()
@@ -28,3 +27,78 @@ func main() {
     fmt.Println("Day 8 Solution (Part 2):")
     fmt.Println("Part 2 execution time:", time.Since(start))
 }
+
+type position struct {
+    xPos int
+    yPos int
+}
+
+func part1(input string) (antis int) {
+    antiNodes := []position{}
+    antennas := make(map[string][]*position)
+
+    for x, line := range strings.Split(input, "\n") {
+        xLimit := len(line)
+        yLimit := len(strings.Split(line, ""))
+        for y, char := range strings.Split(line, "") {
+            if char == "." {
+                continue
+            }
+
+            existingPositions, exists := antennas[char]
+            if exists {
+                for _, existingPos := range existingPositions {
+                    xdiff := existingPos.xPos - x
+                    ydiff := existingPos.yPos - y
+
+                    newXAnti := existingPos.xPos + xdiff
+                    newYAnti := existingPos.yPos + ydiff
+
+                    if  newXAnti < xLimit && 
+                        newXAnti >= 0 && 
+                        newYAnti < yLimit && 
+                        newYAnti >= 0 {
+                        antiNodes = addifunique(antiNodes, existingPos.xPos + xdiff, existingPos.yPos + ydiff)
+                    }
+
+                    newXAnti = x - xdiff
+                    newYAnti = y - ydiff
+                    if  newXAnti < xLimit && 
+                        newXAnti >= 0 && 
+                        newYAnti < yLimit && 
+                        newYAnti >= 0 {
+
+                        antiNodes = addifunique(antiNodes, x - xdiff, y - ydiff)
+                    }
+
+                }
+            } else {
+                fmt.Println("new pos: ", x, y)
+            }
+
+            antennas[char] = append(antennas[char], &position{ xPos: x, yPos: y })
+        }
+    }
+
+    for _, node := range antiNodes {
+        fmt.Println(node.xPos, node.yPos)
+    }
+    return len(antiNodes)
+}
+
+func addifunique(antiNodes []position, newXPos int, newYPos int) ([]position){
+    exists := false
+    for _, p := range antiNodes {
+        if p.xPos == newXPos && p.yPos == newYPos  {
+            exists = true
+            break
+        }
+    }
+
+    if !exists {
+        antiNodes = append(antiNodes, position{ xPos: newXPos, yPos: newYPos })
+    }
+
+    return antiNodes
+}
+
