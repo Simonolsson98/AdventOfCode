17345bd4e63a3561c36ef12ac664b5d271b82475
Simon <simol@student.chalmers.se>
2024-12-09 22:33:23 +0100

    day9 part2 example input works but not real input

--- 2024/day09.go
+++ 2024/day09.go
@@ -109,24 +109,16 @@ func main() {
     thisIndexLol := 0
     sum := 0
     compIndex := 0
-    currentCharIsNotZero := false
     for i, char := range diskMap {
-        if char != "0" {
-            if currentCharIsNotZero {
-                break
-            } else {
-                currentCharIsNotZero = true
-            }
+        if i % 2 == 0 {
             num, _ := strconv.Atoi(char)
             for j := 0; j < num; j++ {
                 sum += (compIndex * (i / 2))
                 compIndex += 1
             }
         } else {
-            currentCharIsNotZero = false
             for _, bruh := range mapOfMovedDiskFiles[thisIndexLol] {
                 sum += (compIndex * bruh)
-
                 compIndex += 1
             } 
             thisIndexLol += 1
@@ -137,7 +129,82 @@ func main() {
     fmt.Println("Part 1 execution time:", time.Since(start), "\n")
 
 	start = time.Now()
-	// exec part2()
-    fmt.Println("Day 9 Solution (Part 2):")
+    fileFreespaceTuple = []test{}
+    diskMap = strings.Split(input, "")
+
+    // extra 0 for the last "pair of values" to represent 0 free spaces
+    if(len(diskMap) % 2 != 0){
+        diskMap = append(diskMap, "0")
+    }
+
+    for i := 0; i < len(diskMap); i+=2 {
+        fileVal, _ := strconv.Atoi(diskMap[i])
+        freespaceVal, _ := strconv.Atoi(diskMap[i + 1])
+        fileFreespaceTuple = append(fileFreespaceTuple, test{file: fileVal, freespace: freespaceVal})
+    }
+    
+    currentPickingIndex = 0
+    mapOfMovedDiskFiles = make(map[int][]int)
+
+    for i := len(fileFreespaceTuple) - 1; i >= 0; i-- {
+        currentPickingIndex = 0
+        val := fileFreespaceTuple[i]
+        fileToMove := val.file
+        for {
+            if currentPickingIndex >= i * 2 {
+                break
+            }
+
+            fitInThis, _ := strconv.Atoi(diskMap[currentPickingIndex + 1])
+
+            if fileToMove <= fitInThis{
+                diskMap[2*i+1] = strconv.Itoa(fileToMove)
+                diskMap[2*i] = "0"
+
+                self, _ := strconv.Atoi(diskMap[currentPickingIndex+1])
+                diskMap[currentPickingIndex+1] = strconv.Itoa(self - fileToMove)
+                
+                for j := 0; j < fileToMove; j++ {
+                    mapOfMovedDiskFiles[currentPickingIndex + 1] = append(mapOfMovedDiskFiles[currentPickingIndex + 1], int(math.Ceil(float64(i))))
+                }
+
+                break
+            }
+
+            currentPickingIndex += 2
+        }
+    }
+
+    thisIndexLol = 0
+    sum = 0
+    compIndex = 0
+    for i, char := range diskMap {
+        if i % 2 == 0 {
+            num, _ := strconv.Atoi(char)
+            if num == 0 {
+                compIndex += 1
+            }
+            for j := 0; j < num; j++ {
+                // fmt.Println("zero, mul: compIndex * bruh", compIndex, "*", (i / 2), "=", compIndex * (i / 2))
+                sum += (compIndex * (i / 2))
+                compIndex += 1
+            }
+        } else {
+            for _, bruh := range mapOfMovedDiskFiles[thisIndexLol] {
+                // fmt.Println("zero, mul: compIndex * bruh", compIndex, "*", bruh, "=", compIndex * bruh)
+                sum += (compIndex * bruh)
+                compIndex += 1
+            } 
+
+            inc, _ := strconv.Atoi(char)
+            // fmt.Println("Skipping dot here: ", char)
+            compIndex += inc
+        }
+
+        thisIndexLol += 1
+    }
+
+    //6478232739671 - 
+    fmt.Println("Day 9 Solution (Part 2):", sum)
     fmt.Println("Part 2 execution time:", time.Since(start))
 }
