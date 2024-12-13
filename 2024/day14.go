4ee0ed1206d93517c717ed1aabbcf7da77427ec3
Simon <simol@student.chalmers.se>
2024-12-13 22:13:23 +0100

    day14 init

---
+++ 2024/day14.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "os"
+    "2024/utils"
+    //"strconv"
+    "path/filepath"
+    "time"
+)
+
+func main() {
+    inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
+    input, err := utils.ReadInput(inputFile)
+    if err != nil {
+        fmt.Println("Error reading input:", err)
+        return
+    }
+
+	start := time.Now()
+	// exec part1()
+    fmt.Println("Day 14 Solution (Part 1):", input)
+    fmt.Println("Part 1 execution time:", time.Since(start), "\n")
+
+	start = time.Now()
+	// exec part2()
+    fmt.Println("Day 14 Solution (Part 2):")
+    fmt.Println("Part 2 execution time:", time.Since(start))
+}
