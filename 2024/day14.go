38cc019969b631c44819310148e23a582ca90ba7
Simon <simol@student.chalmers.se>
2024-12-14 14:02:25 +0100

    day14 part1 refactor + part2 refactor: add user input for fun

--- 2024/day14.go
+++ 2024/day14.go
@@ -11,6 +11,8 @@ import (
     "regexp"
 )
 
+var xrange = 103
+var yrange = 101
 func main() {
     inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
     input, err := utils.ReadInput(inputFile)
@@ -20,65 +22,43 @@ func main() {
     }
 
     start := time.Now()
+    fmt.Println("Day 14 Solution (Part 1):", part1(input))
+    fmt.Println("Part 1 execution time:", time.Since(start), "\n")
 
-    xrange := 103
-    yrange := 101
-    seconds := 100
-    quadrant1 := 0
-    quadrant2 := 0
-    quadrant3 := 0
-    quadrant4 := 0
+    start = time.Now()
+    fmt.Println("Day 14 Solution (Part 2):", part2(input))
+    fmt.Println("Part 2 execution time:", time.Since(start))
+}
+
+func part1(input string) (int){
+    quadrant1 := 0; quadrant2 := 0; quadrant3 := 0; quadrant4 := 0
     for _, line := range strings.Split(input, "\n") {
-        re := regexp.MustCompile(`-?\d+`)
-        matches := re.FindAllStringSubmatch(line, -1)
-        // inverse coordinates for some reason
-        ypos, _ := strconv.Atoi(matches[0][0])
-        xpos, _  := strconv.Atoi(matches[1][0])
-        yvel, _  := strconv.Atoi(matches[2][0])
-        xvel, _  := strconv.Atoi(matches[3][0])
-        // extra mod to prohibit negative numbers, since % in golang allows this..
-        newxPos := ((seconds * xvel + xpos) % xrange + xrange) % xrange
-        newyPos := ((seconds * yvel + ypos) % yrange + yrange) % yrange
+        newxPos, newyPos := calculateNewPositions(line, 100)
 
         if newxPos < xrange / 2{
             if newyPos < yrange/2 {
-                // first quad
                 quadrant1 += 1
             } else if newyPos > yrange/2{
-                // second quad
                 quadrant2 += 1
             }
         } else if newxPos > xrange/2 {
             if newyPos < yrange/2 {
-                // third quad
                 quadrant3 += 1
             } else if newyPos > yrange/2  {
-                // fourth quad
                 quadrant4 += 1
             }
         }
     }
 
-    fmt.Println(quadrant1, quadrant2, quadrant3, quadrant4)
-    fmt.Println("Day 14 Solution (Part 1):", quadrant1 * quadrant2 * quadrant3 * quadrant4)
-    fmt.Println("Part 1 execution time:", time.Since(start), "\n")
+    return quadrant1 * quadrant2 * quadrant3 * quadrant4
+}
 
-	start = time.Now()
-
+func part2(input string) (int){
+    limit := 3
     for i := 0; ; i++ {
         posAfterIter := [][]int{}
         for _, line := range strings.Split(input, "\n") {
-            re := regexp.MustCompile(`-?\d+`)
-            matches := re.FindAllStringSubmatch(line, -1)
-            // inverse coordinates for some reason
-            ypos, _ := strconv.Atoi(matches[0][0])
-            xpos, _  := strconv.Atoi(matches[1][0])
-            yvel, _  := strconv.Atoi(matches[2][0])
-            xvel, _  := strconv.Atoi(matches[3][0])
-            // extra mod to prohibit negative numbers, since % in golang allows this..
-            newxPos := ((i * xvel + xpos) % xrange + xrange) % xrange
-            newyPos := ((i * yvel + ypos) % yrange + yrange) % yrange
-
+            newxPos, newyPos := calculateNewPositions(line, i)
             posAfterIter = append(posAfterIter, []int{newxPos, newyPos})
         }
 
@@ -92,7 +72,7 @@ func main() {
                 }
 
                 // I came up with this condition purely by guessing that a christmas tree should be wide at the bottom...
-                if numOfYInRow > 7 {
+                if numOfYInRow > limit {
                     for a := 0; a < xrange; a++ {
                         for b := 0; b < yrange; b++ {
                             if check(posAfterIter, []int{a, b}){
@@ -104,15 +84,39 @@ func main() {
                         }
                         fmt.Println()
                     }
-                    fmt.Println("FINAL I:", i)
-                    return
+
+                    var ans string
+                    fmt.Println("does this look like a xmas tree to you? (y/n)")
+                    fmt.Scan(&ans)
+                    if ans == "y" {
+                        return i
+                    } else {
+                        limit++
+                        numOfYInRow = 0
+                        break
+                    }
                 }
             }
         }
     }
 
-    fmt.Println("Day 14 Solution (Part 2):")
-    fmt.Println("Part 2 execution time:", time.Since(start))
+    return -1
+}
+
+func calculateNewPositions(line string, seconds int) (int, int) {
+    re := regexp.MustCompile(`-?\d+`)
+    matches := re.FindAllString(line, -1)
+
+    ypos, _ := strconv.Atoi(matches[0])
+    xpos, _ := strconv.Atoi(matches[1])
+    yvel, _ := strconv.Atoi(matches[2])
+    xvel, _ := strconv.Atoi(matches[3])
+
+    // Calculate new positions
+    newxPos := ((seconds * xvel + xpos) % xrange + xrange) % xrange
+    newyPos := ((seconds * yvel + ypos) % yrange + yrange) % yrange
+
+    return newxPos, newyPos
 }
 
 func check(checkThis [][]int, ele []int) (bool){
