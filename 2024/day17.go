d911c38b916398db2efc31376d656033661d7e92
Simon Olsson <Simon.Olsson@omda.com>
2024-12-17 16:02:02 +0100

    day17 part1 done, combooperand != literaloperand...

--- 2024/day17.go
+++ 2024/day17.go
@@ -41,35 +41,38 @@ func main() {
     printthis := ""
     for i := 0; i < len(opcodes); i+=2 {
         instr, _ := strconv.Atoi(opcodes[i])
-        operand, _ := strconv.Atoi(opcodes[i+1])
+        literaloperand, _ := strconv.Atoi(opcodes[i+1])
 
-        if operand == 4 {
-            operand = registers["A"]
-        } else if operand == 5 {
-            operand = registers["B"]
-        } else if operand == 6 {
-            operand = registers["C"]
+        combooperand := literaloperand
+        if literaloperand == 4 {
+            combooperand = registers["A"]
+        } else if literaloperand == 5 {
+            combooperand = registers["B"]
+        } else if literaloperand == 6 {
+            combooperand = registers["C"]
         }
 
         switch instr {
-            case 0: // adv A / 2^operand => A
-                registers["A"] /= int(math.Pow(2.0, float64(operand)))
-            case 1: // B xor operand => B
-                registers["B"] = registers["B"] ^ operand
-            case 2: // operand % 8 => B
-                registers["B"] = (operand % 8 + 8) % 8
+            case 0: // adv A / 2^literaloperand => A
+                registers["A"] = int(float64(registers["A"]) / math.Pow(2.0, float64(combooperand)))
+            case 1: // B xor literaloperand => B
+                registers["B"] = registers["B"] ^ literaloperand
+            case 2: // literaloperand % 8 => B
+                registers["B"] = ((combooperand % 8) + 8) % 8
             case 3: // jump if A != 0
                 if registers["A"] != 0{
-                    i = operand - 2
+                    i = literaloperand - 2
                 }
             case 4: // B XOR C => B
                 registers["B"] = registers["B"] ^ registers["C"]
             case 5: // 
-                printthis = printthis + strconv.Itoa((operand % 8 + 8) % 8) + ","
-            case 6: // bdv A / 2^operand => B
-                registers["B"] = int(float64(registers["A"]) / math.Pow(2.0, float64(operand)))
-            case 7: // cdv A / 2^operand => C
-                registers["C"] = int(float64(registers["A"]) / math.Pow(2.0, float64(operand)))
+                printthis = printthis + strconv.Itoa(((combooperand % 8) + 8) % 8) + ","
+            case 6: // bdv A / 2^literaloperand => B
+                registers["B"] = int(float64(registers["A"]) / math.Pow(2.0, float64(combooperand)))
+            case 7: // cdv A / 2^literaloperand => C
+                registers["C"] = int(float64(registers["A"]) / math.Pow(2.0, float64(combooperand)))
+            default:
+                panic("NOOOOOOOOOOO")
         }
     }
 
