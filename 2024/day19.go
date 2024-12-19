967546ebd3fe3f18180f3fbd949dd4b022bd4a97
Simon Olsson <Simon.Olsson@omda.com>
2024-12-19 11:50:47 +0100

    day19 part1 too slow for real input

--- 2024/day19.go
+++ 2024/day19.go
@@ -10,6 +10,11 @@ import (
     "time"
 )
 
+var (
+    allAvailablePatterns []string
+    sortedStripes = make(map[byte][]string, 0)
+)
+
 func main() {
     inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
     input, err := utils.ReadInput(inputFile)
@@ -18,13 +23,67 @@ func main() {
         return
     }
 
+    splitInput := strings.Split(input, "\n\n")
+    allAvailablePatterns = strings.Split(splitInput[0], ", ")
+    for _, pattern := range allAvailablePatterns {
+        sortedStripes[pattern[0]] = append(sortedStripes[pattern[0]], pattern)
+    }
+
+    towelsToCheck := strings.Split(splitInput[1], "\n")
+    availableTowels := 0
+    
     start := time.Now()
+    for i, towelToCheck := range towelsToCheck {
+        available := checkAll([]string{towelToCheck})
+
+        if !available {
+            fmt.Println("unavailable:", towelToCheck)
+        } else {
+            availableTowels++
+        }
+
+        fmt.Println("i: ", i, "done and available was:", available)
+    }
+
+
     // exec part 1
-    fmt.Println("Day 19 Solution (Part 1):")
+    fmt.Println("Day 19 Solution (Part 1):", availableTowels)
     fmt.Println("Part 1 execution time:", time.Since(start), "\n")
 
     start = time.Now()
     // exec part 2
     fmt.Println("Day 19 Solution (Part 2):")
     fmt.Println("Part 2 execution time:", time.Since(start))
+}
+
+func checkPattern(towel string, pattern string) (bool){
+    return strings.HasPrefix(towel, pattern)
+}
+
+func checkAll(validTowels []string) (bool) {
+    var atleastOnePassing bool = false
+    var newTowels []string
+    for _, validTowel := range validTowels {
+        for _, availablePattern := range sortedStripes[validTowel[0]] {
+            if len(availablePattern) > len(validTowel){
+                continue
+            }
+
+            if checkPattern(validTowel, availablePattern){
+                atleastOnePassing = true
+                newTowel := validTowel[len(availablePattern):]
+                if len(newTowel) == 0{
+                    return true
+                }
+
+                newTowels = append(newTowels, newTowel)
+            }
+        }
+    }
+    
+    if !atleastOnePassing {
+        return false
+    }
+
+    return checkAll(newTowels)
 }
