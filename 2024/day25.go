2df816d560f393e7d69c4fb692a2096a724ef129
Simon Olsson <Simon.Olsson@omda.com>
2024-12-25 12:26:22 +0100

    day25 part1 refactor

--- 2024/day25.go
+++ 2024/day25.go
@@ -34,64 +34,13 @@ func main() {
     var keys []keyOrLock
     var locks []keyOrLock
     for _, in := range strings.Split(input, "\n\n") {
-        var current keyOrLock
-        checkedYs := []int{}
         rows := strings.Split(in, "\n")
         totalLength = len(rows)
-        if strings.Split(rows[0], "")[0] == "#"{ // lock
-            for x, row := range rows {
-                for y, item := range strings.Split(row, "") {
-                    if item == "#" || slices.Contains(checkedYs, y){
-                        continue
-                    }
-                    checkedYs = append(checkedYs, y)
 
-                    switch y {
-                        case 0:
-                            current.col1 = x - 1
-                        case 1:
-                            current.col2 = x - 1
-                        case 2:
-                            current.col3 = x - 1
-                        case 3:
-                            current.col4 = x - 1
-                        case 4:
-                            current.col5 = x - 1
-                        default:
-                            panic("should not be here")
-                    }
-                }
-            }
-
-            locks = append(locks, current)
-        } else { // key
-            checkedYs := []int{}
-            for i := len(rows) - 1; i >= 0; i-- {
-                row := rows[i]
-                for y, item := range strings.Split(row, "") {
-                    if item == "#" || slices.Contains(checkedYs, y){
-                        continue
-                    }
-                    checkedYs = append(checkedYs, y)
-
-                    switch y {
-                        case 0:
-                            current.col1 = len(rows) - i - 2
-                        case 1:
-                            current.col2 = len(rows) - i - 2
-                        case 2:
-                            current.col3 = len(rows) - i - 2
-                        case 3:
-                            current.col4 = len(rows) - i - 2
-                        case 4:
-                            current.col5 = len(rows) - i - 2
-                        default:
-                            panic("should not be here")
-                    }
-                }
-            }
-            
-            keys = append(keys, current)
+        if strings.Split(rows[0], "")[0] == "#"{
+            locks = append(locks, determineLockHeights(rows))
+        } else {
+            keys = append(keys, determineKeyHeights(rows))
         }
     }
 
@@ -102,6 +51,69 @@ func main() {
     // exec part 2
     fmt.Println("Day 25 Solution (Part 2):")
     fmt.Println("Part 2 execution time:", time.Since(start))
+}
+
+func determineLockHeights(rows []string) (keyOrLock){
+    var current keyOrLock
+    checkedYs := []int{}
+
+    for x, row := range rows {
+        for y, item := range strings.Split(row, "") {
+            if item == "#" || slices.Contains(checkedYs, y){
+                continue
+            }
+            checkedYs = append(checkedYs, y)
+
+            switch y {
+                case 0:
+                    current.col1 = x - 1
+                case 1:
+                    current.col2 = x - 1
+                case 2:
+                    current.col3 = x - 1
+                case 3:
+                    current.col4 = x - 1
+                case 4:
+                    current.col5 = x - 1
+                default:
+                    panic("should not be here")
+            }
+        }
+    }
+
+    return current
+}
+
+func determineKeyHeights(rows []string) (keyOrLock){
+    var current keyOrLock
+    checkedYs := []int{}
+
+    for i := totalLength - 1; i >= 0; i-- {
+        row := rows[i]
+        for y, item := range strings.Split(row, "") {
+            if item == "#" || slices.Contains(checkedYs, y){
+                continue
+            }
+            checkedYs = append(checkedYs, y)
+
+            switch y {
+                case 0:
+                    current.col1 = totalLength - i - 2
+                case 1:
+                    current.col2 = totalLength - i - 2
+                case 2:
+                    current.col3 = totalLength - i - 2
+                case 3:
+                    current.col4 = totalLength - i - 2
+                case 4:
+                    current.col5 = totalLength - i - 2
+                default:
+                    panic("should not be here")
+            }
+        }
+    }
+
+    return current
 }
 
 func countKeyLockMatches(keys []keyOrLock, locks []keyOrLock) (int) {
