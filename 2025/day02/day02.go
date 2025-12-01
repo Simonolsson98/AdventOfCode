45dd31a9a7094bbcdbd512d4f2ffd44aac48f060
Simon <98olssi1@gmail.com>
2025-12-01 22:05:57 +0100

    move day01 to separate dir

---
+++ 2025/day02/day02.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/simonolsson98/adventofcode/utils"
+
+	//"strconv"
+	"path/filepath"
+	"time"
+)
+
+func main() {
+	inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
+	input, err := utils.ReadInput(inputFile)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	start := time.Now()
+	result := part1(input)
+	fmt.Println("Day 2 Solution (Part 1):", result)
+	fmt.Println("Part 1 execution time:", time.Since(start), "\n")
+
+	start = time.Now()
+	result = part2(input)
+	fmt.Println("Day 2 Solution (Part 2):", result)
+	fmt.Println("Part 2 execution time:", time.Since(start))
+}
+
+func part1(input string) int {
+
+	return 0
+}
+
+func part2(input string) int {
+
+	return 0
+}
