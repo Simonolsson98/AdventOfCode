b034e1eadcbbe629aa81a88658ed3bb9c7f5c5da
Simon <98olssi1@gmail.com>
2025-12-06 12:20:14 +0100

    day06 part1 done

--- 2025/day06/day06.go
+++ 2025/day06/day06.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "os"
-    "github.com/simonolsson98/adventofcode/utils"
-    //"strconv"
-    "path/filepath"
-    "time"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/simonolsson98/adventofcode/utils"
+
+	"path/filepath"
+	"time"
 )
 
 func main() {
@@ -18,23 +20,54 @@ func main() {
         return
     }
 
+    var grid [][]string
+    for _, line := range strings.Split(input, "\n") {
+        var row []string
+        for _, field := range strings.Fields(line) {
+            row = append(row, field)
+        }
+        grid = append(grid, row)
+    }
+
     start := time.Now()
-    result := part1(input)
+    result := part1(grid)
     fmt.Println("Day 6 Solution (Part 1):", result)
-    fmt.Println("Part 1 execution time:", time.Since(start), "\n")
+    fmt.Println("Part 1 execution time:", time.Since(start))
 
     start = time.Now()
-    result = part2(input)
+    result = part2(grid)
     fmt.Println("Day 6 Solution (Part 2):", result)
     fmt.Println("Part 2 execution time:", time.Since(start))
 }
 
-func part1(input string) int {
-    
-    return 0
+func part1(grid [][]string) int {
+    var total int = 0
+    colSize := len(grid[0])
+    rowSize := len(grid)
+    for i := range colSize {
+        var operation string = grid[rowSize - 1][i] 
+        startingValue := 0
+        if operation == "*" {
+            startingValue = 1
+        }
+            
+        for j := 0; j < rowSize - 1; j++ {
+            val, _ := strconv.Atoi(grid[j][i])
+            
+            switch operation {
+            case "+":
+                startingValue += val
+            case "*":
+                startingValue *= val
+            }
+        }
+
+        total += startingValue
+    }
+    return total
 }
 
-func part2(input string) int {
+func part2(grid [][]string) int {
     
     return 0
 }
