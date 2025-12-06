31bd33980285f702ed232600068531819c800dae
Simon <98olssi1@gmail.com>
2025-12-06 13:29:33 +0100

    day06 part2 done

--- 2025/day06/day06.go
+++ 2025/day06/day06.go
@@ -20,6 +20,20 @@ func main() {
         return
     }
 
+   
+
+    start := time.Now()
+    result := part1(input)
+    fmt.Println("Day 6 Solution (Part 1):", result)
+    fmt.Println("Part 1 execution time:", time.Since(start))
+
+    start = time.Now()
+    result = part2(input)
+    fmt.Println("Day 6 Solution (Part 2):", result)
+    fmt.Println("Part 2 execution time:", time.Since(start))
+}
+
+func part1(input string) int {
     var grid [][]string
     for _, line := range strings.Split(input, "\n") {
         var row []string
@@ -29,18 +43,6 @@ func main() {
         grid = append(grid, row)
     }
 
-    start := time.Now()
-    result := part1(grid)
-    fmt.Println("Day 6 Solution (Part 1):", result)
-    fmt.Println("Part 1 execution time:", time.Since(start))
-
-    start = time.Now()
-    result = part2(grid)
-    fmt.Println("Day 6 Solution (Part 2):", result)
-    fmt.Println("Part 2 execution time:", time.Since(start))
-}
-
-func part1(grid [][]string) int {
     var total int = 0
     colSize := len(grid[0])
     rowSize := len(grid)
@@ -53,7 +55,7 @@ func part1(grid [][]string) int {
             
         for j := 0; j < rowSize - 1; j++ {
             val, _ := strconv.Atoi(grid[j][i])
-            
+
             switch operation {
             case "+":
                 startingValue += val
@@ -67,7 +69,61 @@ func part1(grid [][]string) int {
     return total
 }
 
-func part2(grid [][]string) int {
-    
-    return 0
+func part2(input string) int {
+    rows := strings.Split(input, "\n")
+    rowLength := len(rows)
+    colIndexLength := len(rows[0])
+
+    var total int = 0
+    nums := []int{}
+    for colIndex := colIndexLength - 1; colIndex >= 0; colIndex-- {
+        var digit string = ""
+        for rowIndex := range rowLength {
+            char := rows[rowIndex][colIndex]
+            if string(char) == "" {
+                continue
+            }
+
+            numToAdd , _ := strconv.Atoi(digit)
+
+            // reached operator, so perform operation
+            if strings.TrimSpace(string(char)) == "+" {
+                nums = append(nums, numToAdd)
+                
+                sum := 0
+                for i := 0; i < len(nums); i++ {
+                    numToAdd = nums[i]
+                    sum += numToAdd
+                }
+
+                total += sum
+                digit = ""
+                nums = []int{}
+                break
+            } else if strings.TrimSpace(string(char)) == "*" {
+                nums = append(nums, numToAdd)
+
+                product := 1
+                for i := 0; i < len(nums); i++ {
+                    numToAdd = nums[i]
+                    product *= numToAdd
+                }
+                
+                total += product
+                digit = ""
+                nums = []int{}
+                break
+            }
+
+            digit = digit + strings.TrimSpace(string(char))
+        }
+
+        if strings.TrimSpace(digit) == "" {
+            continue
+        }
+        numToAdd, _ := strconv.Atoi(strings.TrimSpace(digit))
+        nums = append(nums, numToAdd)
+    }
+
+    return total
 }
