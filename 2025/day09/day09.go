2162e11fa086f19b0333cc1761436a837ff9d905
Simon <98olssi1@gmail.com>
2025-12-08 20:17:57 +0100

    day09 init

---
+++ 2025/day09/day09.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "os"
+    "github.com/simonolsson98/adventofcode/utils"
+    //"strconv"
+    "path/filepath"
+    "time"
+)
+
+func main() {
+    inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
+    input, err := utils.ReadInput(inputFile)
+    if err != nil {
+        fmt.Println("Error reading input:", err)
+        return
+    }
+
+    start := time.Now()
+    result := part1(input)
+    fmt.Println("Day 9 Solution (Part 1):", result)
+    fmt.Println("Part 1 execution time:", time.Since(start))
+
+    start = time.Now()
+    result = part2(input)
+    fmt.Println("Day 9 Solution (Part 2):", result)
+    fmt.Println("Part 2 execution time:", time.Since(start))
+}
+
+func part1(input string) int {
+    
+    return 0
+}
+
+func part2(input string) int {
+    
+    return 0
+}
