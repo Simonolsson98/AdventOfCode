fcd6c179b996beab0153b487840faa312cb28d04
Simon Olsson <Simon.Olsson@omda.com>
2025-12-09 07:49:39 +0100

    day9 part1 done

--- 2025/day09/day09.go
+++ 2025/day09/day09.go
@@ -1,42 +1,69 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "os"
-    "github.com/simonolsson98/adventofcode/utils"
-    //"strconv"
-    "path/filepath"
-    "time"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/simonolsson98/adventofcode/utils"
 )
 
 func main() {
-    inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
-    input, err := utils.ReadInput(inputFile)
-    if err != nil {
-        fmt.Println("Error reading input:", err)
-        return
-    }
+	inputFile := strings.Split(filepath.Base(os.Args[0]), ".")[0] + "_input"
+	input, err := utils.ReadInput(inputFile)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
 
-    start := time.Now()
-    result := part1(input)
-    elapsed := time.Since(start)
-    fmt.Println("Day 9 Solution (Part 1):", result)
-    fmt.Printf("Part 1 execution time: %.2fµs\n", float64(elapsed.Nanoseconds())/1000.0)
+	start := time.Now()
+	result := part1(input)
+	elapsed := time.Since(start)
+	fmt.Println("Day 9 Solution (Part 1):", result)
+	fmt.Printf("Part 1 execution time: %.2fµs\n", float64(elapsed.Nanoseconds())/1000.0)
 
-    start = time.Now()
-    result = part2(input)
-    elapsed = time.Since(start)
-    fmt.Println("Day 9 Solution (Part 2):", result)
-    fmt.Printf("Part 2 execution time: %.2fµs\n", float64(elapsed.Nanoseconds())/1000.0)
+	start = time.Now()
+	result = part2(input)
+	elapsed = time.Since(start)
+	fmt.Println("Day 9 Solution (Part 2):", result)
+	fmt.Printf("Part 2 execution time: %.2fµs\n", float64(elapsed.Nanoseconds())/1000.0)
 }
 
 func part1(input string) int {
-    
-    return 0
+	max := 0
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		for _, line2 := range strings.Split(strings.TrimSpace(input), "\n") {
+			if line == line2 {
+				continue
+			}
+
+			coords := strings.Split(line, ",")
+			coords2 := strings.Split(line2, ",")
+			x, _ := strconv.Atoi(coords[0])
+			y, _ := strconv.Atoi(coords[1])
+			x2, _ := strconv.Atoi(coords2[0])
+			y2, _ := strconv.Atoi(coords2[1])
+
+			xdiff := x - x2
+			ydiff := y - y2
+			if xdiff < 0 {
+				xdiff = -xdiff
+			}
+			if ydiff < 0 {
+				ydiff = -ydiff
+			}
+			if (xdiff+1)*(ydiff+1) > max {
+				max = (xdiff + 1) * (ydiff + 1)
+			}
+		}
+	}
+	return max
 }
 
 func part2(input string) int {
-    
-    return 0
+
+	return 0
 }
