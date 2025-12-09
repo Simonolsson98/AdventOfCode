0691b17c25688bd77807e56cd0d36cae992512d1
Simon Olsson <Simon.Olsson@omda.com>
2025-12-09 13:20:37 +0100

    day9 part2 done with 15 min runtime LOL

--- 2025/day09/day09.go
+++ 2025/day09/day09.go
@@ -63,7 +63,177 @@ func part1(input string) int {
 	return max
 }
 
-func part2(input string) int {
+type Pair struct {
+	x, y int
+}
 
-	return 0
+func part2(input string) int {
+	corners := strings.Split(strings.TrimSpace(input), "\n")
+	var hSegments []HSegment
+	var vSegments []VSegment
+
+	for _, corner := range corners {
+		currentCorner := strings.Split(corner, ",")
+		currentCornerXCoord, _ := strconv.Atoi(currentCorner[0])
+		currentCornerYCoord, _ := strconv.Atoi(currentCorner[1])
+
+		corners2 := strings.Split(strings.TrimSpace(input), "\n")
+		for _, corner2 := range corners2 {
+			if corner2 == corner {
+				continue
+			}
+
+			otherCorner := strings.Split(corner2, ",")
+			otherCornerXCoord, _ := strconv.Atoi(otherCorner[0])
+			otherCornerYCoord, _ := strconv.Atoi(otherCorner[1])
+			if otherCornerXCoord == currentCornerXCoord { // matching x coord
+				y1 := min(currentCornerYCoord, otherCornerYCoord)
+				y2 := max(currentCornerYCoord, otherCornerYCoord)
+				vSegments = append(vSegments, VSegment{currentCornerXCoord, y1, y2})
+			} else if otherCornerYCoord == currentCornerYCoord { // matching y coord
+				x1 := min(currentCornerXCoord, otherCornerXCoord)
+				x2 := max(currentCornerXCoord, otherCornerXCoord)
+				hSegments = append(hSegments, HSegment{currentCornerYCoord, x1, x2})
+			}
+		}
+	}
+
+	maxArea := 0
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		for _, line2 := range strings.Split(strings.TrimSpace(input), "\n") {
+			if line == line2 {
+				continue
+			}
+
+			coords := strings.Split(line, ",")
+			coords2 := strings.Split(line2, ",")
+			x, _ := strconv.Atoi(coords[0])
+			y, _ := strconv.Atoi(coords[1])
+			x2, _ := strconv.Atoi(coords2[0])
+			y2, _ := strconv.Atoi(coords2[1])
+
+			xdiff := x - x2
+			ydiff := y - y2
+			if xdiff < 0 {
+				xdiff = -xdiff
+			}
+			if ydiff < 0 {
+				ydiff = -ydiff
+			}
+			if (xdiff+1)*(ydiff+1) > maxArea {
+				xMin := min(x, x2)
+				xMax := max(x, x2)
+				yMin := min(y, y2)
+				yMax := max(y, y2)
+
+				valid := true
+				for i := xMin; i <= xMax; i++ {
+					if !hasPointBelow(i, yMin, hSegments, vSegments) {
+						valid = false
+						break
+					}
+					if !hasPointAbove(i, yMax, hSegments, vSegments) {
+						valid = false
+						break
+					}
+				}
+				if valid {
+					for j := yMin; j <= yMax; j++ {
+						if !hasPointLeft(xMin, j, hSegments, vSegments) {
+							valid = false
+							break
+						}
+						if !hasPointRight(xMax, j, hSegments, vSegments) {
+							valid = false
+							break
+						}
+					}
+				}
+
+				if valid {
+					maxArea = (xdiff + 1) * (ydiff + 1)
+				}
+			}
+		}
+	}
+
+	return maxArea
+}
+
+type HSegment struct {
+	y, x1, x2 int
+}
+
+type VSegment struct {
+	x, y1, y2 int
+}
+
+func hasPointBelow(x, y int, hSegs []HSegment, vSegs []VSegment) bool {
+	for _, s := range vSegs {
+		if s.x == x && s.y1 <= y {
+			return true
+		}
+	}
+	for _, s := range hSegs {
+		if s.y <= y && x >= s.x1 && x <= s.x2 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasPointAbove(x, y int, hSegs []HSegment, vSegs []VSegment) bool {
+	for _, s := range vSegs {
+		if s.x == x && s.y2 >= y {
+			return true
+		}
+	}
+	for _, s := range hSegs {
+		if s.y >= y && x >= s.x1 && x <= s.x2 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasPointLeft(x, y int, hSegs []HSegment, vSegs []VSegment) bool {
+	for _, s := range hSegs {
+		if s.y == y && s.x1 <= x {
+			return true
+		}
+	}
+	for _, s := range vSegs {
+		if s.x <= x && y >= s.y1 && y <= s.y2 {
+			return true
+		}
+	}
+	return false
+}
+
+func hasPointRight(x, y int, hSegs []HSegment, vSegs []VSegment) bool {
+	for _, s := range hSegs {
+		if s.y == y && s.x2 >= x {
+			return true
+		}
+	}
+	for _, s := range vSegs {
+		if s.x >= x && y >= s.y1 && y <= s.y2 {
+			return true
+		}
+	}
+	return false
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
